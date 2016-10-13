67cc4f8d6834715d6f7f484be98585b0e36f60c5
DOCaCola <docacola@fleetops.net>
2016-10-13 20:15:28 +0200

    Changes for new site layout

--- spiegelplusremover.user.js
+++ spiegelplusremover.user.js
@@ -1,25 +1,23 @@
 // ==UserScript==
 // @name         Spiegel Plus remover
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Remove any Spiegel Plus content and reference from the site
+// @version      0.2
+// @description  Spiegel Plus und Bento links von spiegel.de entfernen
 // @author       DOCa Cola
 // @match        *://*.spiegel.de/*
 // @grant        none
+// @noframes
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
 function removeSPPlus(selector)
 {
-    //$(selector).css("opacity","0.2");
     $(selector).remove();
 }
 
-$(document).ready(function () {
-    removeSPPlus('.teaser:has(.laterpay-icon-big)');
-    removeSPPlus('.ressort-teaser-box-top:has(.laterpay-icon-big)');
-    removeSPPlus('.pano_xxl .asset-box:has(.laterpay-icon)');
-    removeSPPlus('.spplusboxhp');
-    removeSPPlus('li:has(.laterpay-icon)');
-    removeSPPlus('.module-box.spiegelplus');
-});
+$(document).ready(function() {
+    removeSPPlus('.asset-box:has(.spiegelplus)');
+    removeSPPlus('.teaser:has(.spiegelplus)');
 
+    removeSPPlus('.teaser:has(.bento)');
+});
