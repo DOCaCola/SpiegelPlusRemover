4c7a7edbf2f3afaa997e62d7b5899d17fd2315bb
DOCaCola <docacola@fleetops.net>
2016-11-07 23:17:57 +0100

    Remove inline links and module boxes

--- spiegelplusremover.user.js
+++ spiegelplusremover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spiegel Plus remover
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.5
 // @description  Spiegel Plus und Bento links von spiegel.de entfernen
 // @author       DOCa Cola
 // @match        *://*.spiegel.de/*
@@ -15,12 +15,22 @@ function removeSPPlus(selector)
     $(selector).remove();
 }
 
+function linkRemover(selector)
+{
+    var element = $(selector);
+    var htmlString = element.contents();
+    element.replaceWith(htmlString);
+}
+
 $(document).ready(function() {
     removeSPPlus('.article-list:has(.spiegelplus)');
     removeSPPlus('.asset-box:has(.spiegelplus)');
     removeSPPlus('.teaser:has(.spiegelplus)');
+    removeSPPlus('.module-box.spiegelplus');
+    linkRemover('.text-link-int.spiegelplus');
     // bento
     removeSPPlus('.article-list:has(.bento)');
     removeSPPlus('.asset-box:has(.bento)');
     removeSPPlus('.teaser:has(.bento)');
+    removeSPPlus('.module-box.bento');
 });
